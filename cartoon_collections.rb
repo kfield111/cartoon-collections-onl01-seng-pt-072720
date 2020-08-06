e4065dcd76b67122fc1e213b95a04c4c24402dc2
Kelin Field <kfield111@hotmail.com>
2020-08-06 01:59:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls (array)
   array.each do |word|
     if word.length > 4
       return true
-    else return false
+    elseif word.length !> 4
+     return false
     end
     end
 end

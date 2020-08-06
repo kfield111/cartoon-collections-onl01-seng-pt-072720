73553af3e335ef5552c731fbdd7909bc9a19db10
Kelin Field <kfield111@hotmail.com>
2020-08-06 02:18:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls (array)
     array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
+  array.detect{ |name| name.include(cheese_types)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end

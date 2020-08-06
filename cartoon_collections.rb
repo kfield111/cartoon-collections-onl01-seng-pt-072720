e92afe7dfc1f5263b8a2b193c6fcd87b5d42e33a
Kelin Field <kfield111@hotmail.com>
2020-08-06 01:52:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,12 @@ def summon_captain_planet (array)
   array.map! {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  i = 0
+  while i < array.length
+    if yield array.include? (word.length > 4)
+      return true
+    end
 end
 
 def find_the_cheese# code an argument here

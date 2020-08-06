3df856c6dda027141d9daee6278976b73d111efa
Kelin Field <kfield111@hotmail.com>
2020-08-06 02:21:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  yield array.detect{ |name| name.include?("#{cheese_types}")}
+  array.detect{ |name| name.include?("#{cheese_types}")}
 end

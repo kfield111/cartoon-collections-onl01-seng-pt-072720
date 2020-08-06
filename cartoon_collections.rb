7607b9d51c68b06988cf250f894d949fda36fb17
Kelin Field <kfield111@hotmail.com>
2020-08-06 02:19:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def long_planeteer_calls (array)
 end
 
 def find_the_cheese (array)
-  array.detect{ |name| name.include?(cheese_types)}
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect{ |name| name.include?(cheese_types)}
+  
 end
